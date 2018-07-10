04a97ce97ee1bd92c3665e5c40990cb1497a196b
Thuan Nguyen <nguyenvanthuan.vnit@gmail.com>
2018-07-10 14:33:30 +0700

    Fixed buffers size

--- chat-server/connection/connection.go
+++ chat-server/connection/connection.go
@@ -17,8 +17,8 @@ var (
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  4024,
-	WriteBufferSize: 4024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
